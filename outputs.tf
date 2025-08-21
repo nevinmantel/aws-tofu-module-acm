500d49030ef5b3f3077e53f553923b0292dfc465
nevinmantel <vinod.nelatur@mantelgroup.com.au>
2025-08-21 15:46:45 +1000

    remove auto validatoin

--- outputs.tf
+++ outputs.tf
@@ -12,22 +12,29 @@ output "manual_dns_validation_records" {
   description = <<EOT
 DNS records that must be created manually to validate ACM certificates.
 
-⚠️ IMPORTANT: Add these CNAME records to your DNS provider within 24 hours 
+⚠️ IMPORTANT: Add these CNAME records to your DNS provider within 72 hours 
 or the ACM certificate request will fail.
 EOT
 
   value = {
-    for k, v in aws_acm_certificate.this :
-    k => v.domain_validation_options
-    if var.certs[k].validation_method == "manual"
+    for cert_key, cert in aws_acm_certificate.this :
+    cert_key => {
+      for opt in cert.domain_validation_options :
+      opt.resource_record_name => opt.resource_record_value
+    }
+    if var.certs[cert_key].validation_method == "manual"
   }
 }
 
-# DNS records that were auto-created
+# DNS records that were auto-created in Route53
 output "automatic_dns_validation_records" {
+  description = "DNS validation records automatically created in Route53"
   value = {
-    for k, v in aws_acm_certificate.this :
-    k => v.domain_validation_options
-    if var.certs[k].validation_method == "automatic"
+    for cert_key, cert in aws_acm_certificate.this :
+    cert_key => {
+      for opt in cert.domain_validation_options :
+      opt.resource_record_name => opt.resource_record_value
+    }
+    if var.certs[cert_key].validation_method == "automatic"
   }
 }
