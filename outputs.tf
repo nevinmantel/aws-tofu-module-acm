488dfe8331755a7d9be36ce6e839067d9a8854eb
nevinmantel <vinod.nelatur@mantelgroup.com.au>
2025-08-21 17:12:52 +1000

    changes

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,15 @@
 # ARNs for automatically validated certificates
 output "pending_validation_records" {
-  description = "DNS records required to validate ACM certificates. Add these manually or use Route53 for automatic validation later."
+  description = "DNS records required to validate ACM certificates. Useful for manual validation or debugging."
   value = {
-    for k, v in aws_acm_certificate.this :
-    k => tolist(v.domain_validation_options)
+    for k, cert in aws_acm_certificate.this :
+    k => [for opt in cert.domain_validation_options : {
+      domain_name           = opt.domain_name
+      validation_domain     = opt.domain_validation_domain
+      resource_record_name  = opt.resource_record_name
+      resource_record_type  = opt.resource_record_type
+      resource_record_value = opt.resource_record_value
+    }]
   }
 }
 
@@ -17,10 +23,8 @@ or the ACM certificate request will fail.
 EOT
 
   value = {
-    for cert_key, cert in aws_acm_certificate.this :
-    cert_key => {
-      for opt in cert.domain_validation_options :
-      opt.resource_record_name => opt.resource_record_value
+    for cert_key, cert in aws_acm_certificate.this : cert_key => {
+      for opt in cert.domain_validation_options : opt.resource_record_name => opt.resource_record_value
     }
     if var.certs[cert_key].validation_method == "manual"
   }
